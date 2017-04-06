1860edcc940fdefa5011553e2d88e54ccaff263e
Emzi0767 <matiz100@windowslive.com>
2017-04-06 14:20:03 +0200

    Added logic tests.

---
+++ Emzi0767.OperatorList.Test/LogicTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Emzi0767
+{
+    [TestClass]
+    [TestCategory("Logic")]
+    public class LogicTests
+    {
+        [TestMethod]
+        public void TestLogic1()
+        {
+            var list1 = new OperatorList<int>(new[] { 1, 2, 3 });
+            var list2 = new OperatorList<int>();
+            var list3 = (OperatorList<int>)null;
+
+            if (list1) { }
+            else
+                Assert.Fail();
+            if (list2)
+                Assert.Fail();
+            if (list3)
+                Assert.Fail();
+        }
+    }
+}
