14ed2026b5de7a9d012bf297520010355d7e7887
Emzi0767 <matiz100@windowslive.com>
2017-04-06 15:03:32 +0200

    Added equality tests.

---
+++ Emzi0767.OperatorList.Test/EqualityTests.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Emzi0767
+{
+    [TestClass]
+    [TestCategory("Equality")]
+    public class EqualityTests
+    {
+        [TestMethod]
+        public void TestEquality1()
+        {
+            var list1 = new OperatorList<int>(new[] { 1, 2, 3 });
+            var list2 = new OperatorList<int>(new[] { 1, 2, 3 });
+            var list3 = new OperatorList<int>(new[] { 1, 2, 4 });
+            var list4 = new OperatorList<int>();
+
+            Assert.IsTrue(list1 == list2);
+            Assert.IsFalse(list1 == list3);
+            Assert.IsFalse(list1 == list4);
+            Assert.IsFalse(list2 == list3);
+            Assert.IsFalse(list3 == list4);
+            Assert.IsFalse(list1 == null);
+            Assert.IsFalse(list2 == null);
+            Assert.IsFalse(list3 == null);
+            Assert.IsFalse(list4 == null);
+        }
+
+        [TestMethod]
+        public void TestEquality2()
+        {
+            var list1 = new OperatorList<int>(new[] { 1, 2, 3 });
+            var list2 = new OperatorList<int>(new[] { 1, 2, 3 });
+            var list3 = new OperatorList<int>(new[] { 1, 2, 4 });
+            var list4 = new OperatorList<int>();
+
+            Assert.IsFalse(list1 != list2);
+            Assert.IsTrue(list1 != list3);
+            Assert.IsTrue(list1 != list4);
+            Assert.IsTrue(list2 != list3);
+            Assert.IsTrue(list3 != list4);
+            Assert.IsTrue(list1 != null);
+            Assert.IsTrue(list2 != null);
+            Assert.IsTrue(list3 != null);
+            Assert.IsTrue(list4 != null);
+        }
+    }
+}
---
+++ Emzi0767.OperatorList.Test/GreatnessTests.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Emzi0767
+{
+    [TestClass]
+    [TestCategory("Greatness")]
+    public class GreatnessTests
+    {
+        [TestMethod]
+        public void TestGreatness1()
+        {
+            var list1 = new OperatorList<int>(new[] { 1, 2, 3 });
+            var list2 = new OperatorList<int>(new[] { 1, 2, 3 });
+            var list3 = new OperatorList<int>(new[] { 1, 2, 3, 4 });
+            var list4 = new OperatorList<int>(new[] { 1, 2 });
+
+            Assert.IsFalse(list1 > list2);
+            Assert.IsTrue(list1 >= list2);
+
+            Assert.IsFalse(list1 > list3);
+            Assert.IsFalse(list1 >= list3);
+
+            Assert.IsTrue(list1 > list4);
+            Assert.IsTrue(list1 >= list4);
+        }
+
+        [TestMethod]
+        public void TestGreatness2()
+        {
+            var list1 = new OperatorList<int>(new[] { 1, 2, 3 });
+            var list2 = new OperatorList<int>(new[] { 1, 2, 3 });
+            var list3 = new OperatorList<int>(new[] { 1, 2, 3, 4 });
+            var list4 = new OperatorList<int>(new[] { 1, 2 });
+
+            Assert.IsFalse(list1 < list2);
+            Assert.IsTrue(list1 <= list2);
+
+            Assert.IsTrue(list1 < list3);
+            Assert.IsTrue(list1 <= list3);
+
+            Assert.IsFalse(list1 < list4);
+            Assert.IsFalse(list1 <= list4);
+        }
+    }
+}
